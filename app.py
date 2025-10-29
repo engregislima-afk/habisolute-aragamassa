a960bc6a75fcb860c6bcd33df70e3514906dd5b8
engregislima-afk <eng.regislima@gmail.com>
2025-10-29 13:14:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -152,10 +152,7 @@ div[data-testid="stToolbar"]:hover{
 """, unsafe_allow_html=True)
 
 # ===== Título
-st.markdown(
-    "<div id='app-title-wrap'><h1 id='app-title' style='margin:0'>🏗️Sistema de Rupturas de Argamassa Habisolute</h1></div>",
-    unsafe_allow_html=True
-)
+st.markdown("<h1 id='app-title' style='margin:0'>🏗️Sistema de Rupturas de Argamassa Habisolute</h1>", unsafe_allow_html=True)
 st.caption("Entrada: **carga (kgf)**. Saídas: **kN/cm²** e **MPa**. PDF direto em 1 clique (somente fpdf2).")
 # === HOTFIX FINAL (coloque no FIM do arquivo) ===
 st.markdown("""
